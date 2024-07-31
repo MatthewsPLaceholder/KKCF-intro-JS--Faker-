a02c16ca56063cda34658da99f9ce50d094d00de
MatthewsPLaceholder <theworkingplaceholder@gmail.com>
2024-07-31 16:36:35 +0000

    js examples

--- script.js
+++ script.js
@@ -1,7 +1,22 @@
 
 //console log text, expression 
+//console.log("hello world!") 
+//console.log(69+420)
+// Create a variable to hold your name as a string  
+//let myName = "Matt" 
+//myName = "Judas"  //reassign value of myName
+//console.log(myName + MyMessage)
 
-// Create a variable to hold your name as a string
+
+//const myName = "Matt"
+
+//var myName = "Matt" 
+
+//let myName = "Matt" 
+//let MyMessage = "Good Morning!" 
+
+//console.log(myName + MyMessage)
+
 
 //Concatenate your name variable into a message and log it to the console.
 //Example: “Hello, Zach!” 
@@ -11,13 +26,26 @@
 //Re-assign the value of your name variable below its declaration and console log it again
 
 //practice math operators
+//console.log(69+420) 
+//console.log(69-420) 
+//console.log(69*420) 
+//console.log(69/420)   
+ 
+
+//let myNumber = 6**2
 
 // boolean - true or false
-
+let isFRFR = true  
+//console.log(isFRFR)
 //create null and undefined
-
+//console.log(isnice)
 //mix data types
+//console.log = ("69" + 420)
 
+let quantity = "12"; 
+console.log(type of quantity);
+quantity = Number(quantity) 
+console.log(type of quantity);
 //string methods
 
 //link up html file and create prompt and save value
